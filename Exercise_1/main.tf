f03877c465d86b372fc1db7186977c2995e2e149
Phuoc Dinh Thien <dinhthienphuoc1990@gmail.com>
2023-12-02 07:21:07 +0700

    terraform: updated code

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,36 @@
-# TODO: Designate a cloud provider, region, and credentials
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
 
+  required_version = ">= 0.14.9"
+}
+
+# TODO: Designate a cloud provider, region, and credentials
+provider "aws" {
+  profile = "uda-ca-per-scale"
+  region  = "us-east-1"
+}
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-
+resource "aws_instance" "Udacity_T2" {
+  count         = "4"
+  ami           = "ami-0fc5d935ebf8bc3bc"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Udacity T2"
+  }
+}
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+# resource "aws_instance" "Udacity_M4" {
+#   count         = "2"
+#   ami           = "ami-0fc5d935ebf8bc3bc"
+#   instance_type = "m4.large"
+#   tags = {
+#     Name = "Udacity M4"
+#   }
+# }
--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,1 +1,10 @@
 # TODO: Define the variable for aws_region
+variable "aws_region" {
+  default = "us-east-1"
+}
+variable "lambda_name" {
+  default = "greet_lambda"
+}
+variable "lambda_output_path" {
+  default = "output.zip"
+}
